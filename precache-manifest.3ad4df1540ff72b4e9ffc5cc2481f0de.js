cb0b459e1797e7bd2d1bbac5800dd64fada1b9d0
Seth Mak <mak.seth@gmail.com>
2020-04-13 18:42:57 +0800

    Updates

---
+++ precache-manifest.3ad4df1540ff72b4e9ffc5cc2481f0de.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e1bb30870325c833fdabd485515b0f3",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "ca41dc324ed545b03e37",
+    "url": "/monsters-rolodex/static/css/main.90b12433.chunk.css"
+  },
+  {
+    "revision": "b677585a46edb6ec1c30",
+    "url": "/monsters-rolodex/static/js/2.5571cc8b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.5571cc8b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca41dc324ed545b03e37",
+    "url": "/monsters-rolodex/static/js/main.f5f9f03c.chunk.js"
+  },
+  {
+    "revision": "6a08449d174cf0268ea4",
+    "url": "/monsters-rolodex/static/js/runtime-main.62dd08d8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monsters-rolodex/static/media/logo.5d5d9eef.svg"
+  }
+]);
